45eb2bd12b4b17c9868324d2af68a91ebd9916ed
rivacortezf <u202215313@upc.edu.pe>
2024-10-03 16:33:01 -0500

    feat: fix i18n problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ToolbarContentComponent} from './public/components/toolbar-content/toolbar-content.component';
+import { ToolbarContentComponent } from './public/components/toolbar-content/toolbar-content.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ToolbarContentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Eventify';
+
+  constructor(private translate: TranslateService) {
+
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.setDefaultLang('en');
+
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,48 +1,32 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {HttpClient, provideHttpClient} from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
+// Función para cargar archivos de traducción desde el servidor
 export function HttpLoaderFactory(http: HttpClient) {
-
   return new TranslateHttpLoader(http);
-
 }
 
-
-
 export const appConfig: ApplicationConfig = {
-
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-
     provideAnimationsAsync(),
-
     provideHttpClient(),
-
     importProvidersFrom(
-
       TranslateModule.forRoot({
-
         loader: {
-
           provide: TranslateLoader,
-
           useFactory: HttpLoaderFactory,
-
           deps: [HttpClient]
-
-        }
-
+        },
+        defaultLanguage: 'en',  // Establecer idioma por defecto aquí
+        useDefaultLang: true    // Garantizar uso de idioma predeterminado
       })
-
     )
-
   ]
-
 };
